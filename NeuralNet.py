d4c3ab84d772622c937c6646fcc91f4f8246ea55
Saksham Singh <SakshamSinghKasana@users.noreply.github.com>
2026-08-27 22:50:02 +0530

    Demo NeuralNetwork2

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,32 +1,34 @@
+import math
+
 class Neuron:
     def __init__(self, inputX, W_list):
         self.inputX = inputX
         self.W_list = W_list
 
-    def z(W_list,inputX):
-        sum = 0
-        for i in range(len(W_list)):
-            sum += W_list*inputX
-        return sum
+    def z(self):
+        total = 0
+        for i in range(len(self.W_list)):
+            total += self.W_list[i] * self.inputX[i]
+        return total
 
-    def a(z):
-        return 1/(1+(2.718**(-z)))
+    def a(self):
+        return 1 / (1 + (math.e**(-self.z())))
 
 def main():
-    inputX = [1,2,3,4]
-    W1 = [[1,2,3,4],[1,2,3,4]]
+    inputX = [1, 2, 3, 4]
+    W1 = [
+            [1, 2, 3, 4],
+            [1, 2, 3, 4]
+    ]
     W2 = [1,2]
+    l11 = Neuron(inputX, W1[0]).a()
+    l12 = Neuron(inputX, W1[1]).a()
 
-    l01 = inputX[0]
-    l02 = inputX[1]
-    l03 = inputX[2]
-    l04 = inputX[3]
+    l1 = [l11, l12]
+    l21 = Neuron(l1,W2).a()
+    output = [l21]
 
-    l11 = Neuron(inputX, W_list)
-    l12 = Neuron(inputX, W_list)
+    print(output)
 
-    l0 = [l01,l02,l03,l04]
-    l1 = [l11,l12]
-    l = [l0,l1]
-
-    for i in 
+if __name__ == "__main__":
+    main()
